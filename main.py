3973253132f87b1a48d40ac5fa59bc3ce0fd2129
nbdaya <perfilprofissionalegn@gmail.com>
2026-02-25 13:17:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -208,13 +208,6 @@ def logout():
     return redirect(url_for('splash'))
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
 
 
 
