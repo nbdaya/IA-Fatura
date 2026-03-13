86e62600936e9d5302a58a67e84a3eaa5e6d11e5
nbdaya <perfilprofissionalegn@gmail.com>
2026-03-13 15:58:33 -0300

    Update detector.py

--- parser/detector.py
+++ parser/detector.py
@@ -1,1 +1,23 @@
+def detectar_concessionaria(texto):
 
+    texto = texto.upper()
+
+    if "ENEL" in texto:
+        return "ENEL"
+
+    if "NEOENERGIA" in texto:
+        return "NEOENERGIA"
+
+    if "EQUATORIAL" in texto:
+        return "EQUATORIAL"
+
+    if "CPFL" in texto:
+        return "CPFL"
+
+    if "ENERGISA" in texto:
+        return "ENERGISA"
+
+    if "CEMIG" in texto:
+        return "CEMIG"
+
+    return "DESCONHECIDA"
