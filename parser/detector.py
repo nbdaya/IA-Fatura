8300a2554fc93387641d23bab90eb6912f0c0c05
nbdaya <perfilprofissionalegn@gmail.com>
2026-03-17 15:41:40 -0300

    Update detector.py

--- parser/detector.py
+++ parser/detector.py
@@ -1,25 +1,17 @@
 def detectar_concessionaria(texto):
 
-    texto = texto.upper()
+    texto = texto.lower()
 
-    if "ENEL" in texto:
-        return "ENEL"
+    mapa = {
+        "LIGHT": ["light", "light servicos de eletricidade"],
+        "EQUATORIAL": ["equatorial"],
+        "NEOENERGIA": ["neoenergia", "coelba", "celpe", "cosern"],
+        "ENEL": ["enel", "ampla"],
+    }
 
-    if "NEOENERGIA" in texto:
-        return "NEOENERGIA"
-
-    if "EQUATORIAL" in texto:
-        return "EQUATORIAL"
-
-    if "CPFL" in texto:
-        return "CPFL"
-
-    if "ENERGISA" in texto:
-        return "ENERGISA"
-
-    if "CEMIG" in texto:
-        return "CEMIG"
-    if "LIGHT": ["light", "light servicos de eletricidade"] in texto:
-        return "LIGHT"
+    for nome, palavras in mapa.items():
+        for palavra in palavras:
+            if palavra in texto:
+                return nome
 
     return "DESCONHECIDA"
